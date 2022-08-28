73c504f6d36d479846b10cfe0f9c72f0b537a7ab
phoebeinviere <phoebeinviere@gmail.com>
2022-08-28 00:10:05 -0400

    3 files modified

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+/*
 //Creating a 1D array
 var flowerTerp = new Array(10);
-
-document.write('Creating 2D array <br>');
 
 // Loop to create 2D array using the 1D array
 for (var i = 0; i < flowerTerp.length; i++) {
@@ -11,10 +10,9 @@ for (var i = 0; i < flowerTerp.length; i++) {
 //Filling the 2D array
 flowerTerp[0] = [0.72, 0.51, 0.23, 0.11, 0.09, 0.07, 0.04, 0.13, 0.03, 0.01, 0, 0, 0, 0, 0, 0];
 flowerTerp[1]= [0.72, 0.90, 0.21, 0.03, 0.16, 0.10, 0.18, 0.02, 0.04, 0.02, 0.09, 0.03, 0, 0, 0, 0];
+*/
 
-
-
-/* Loop to display the elements of 2D array.
+/*Loop to display the elements of 2D array.
 for (var i= 0; i < 2; i++) {
   for (var j = 0; j < 16; j++) {
     document.write(flowerTerp[i][j] + ' ');
