44ff19f9961187da27c6a4ba5fcd9e4014845250
phoebeinviere <phoebeinviere@gmail.com>
2022-08-27 23:38:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ykht3c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ykht3c)
---
+++ index.js
@@ -0,0 +1,35 @@
+//Creating a 1D array
+var flowerTerp = new Array(10);
+
+document.write('Creating 2D array <br>');
+
+// Loop to create 2D array using the 1D array
+for (var i = 0; i < flowerTerp.length; i++) {
+  flowerTerp[i] = new Array(16);
+}
+
+//Filling the 2D array
+flowerTerp[0] = [0.72, 0.51, 0.23, 0.11, 0.09, 0.07, 0.04, 0.13, 0.03, 0.01, 0, 0, 0, 0, 0, 0];
+flowerTerp[1]= [0.72, 0.90, 0.21, 0.03, 0.16, 0.10, 0.18, 0.02, 0.04, 0.02, 0.09, 0.03, 0, 0, 0, 0];
+
+/* Loop to display the elements of 2D array.
+for (var i= 0; i < 2; i++) {
+  for (var j = 0; j < 16; j++) {
+    document.write(flowerTerp[i][j] + ' ');
+  }
+  document.write('<br>');
+}
+*/
+
+/* let table = document.createElement('table');
+let row = table.insertRow();
+let cell = row.insertCell();
+for (let row of flowerTerp) {
+  table.insertRow();
+  for (let cell of row) {
+    let newCell = table.rows[table.rows.length - 1].insertCell();
+    newCell.textContent = cell;
+  }
+}
+document.body.appendChild(table);
+*/
