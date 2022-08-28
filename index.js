9708d0db02f3c57587089c524aad74ae912d64a5
phoebeinviere <phoebeinviere@gmail.com>
2022-08-28 01:41:39 -0400

    1 file modified

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ $(document).ready(function () {
   $('tbody tr td').each(
 
     function() {
-        var fTColor1 = 0.02,
-            fTColor2 = 0.04,
-            good = 25,
-            avg = 20,
-            poor = 15,
-            vPoor = 10,
-            score = $(this).text();
+        var fTcolor1 = 0.02,
+            fTcolor2 = 0.04,
+            fTcolor3 = 0.06,
+            fTcolor4 = 0.08,
+            fTcolor5 = 0.10,
+            fTcolor6 = 0.12,
+            value = $(this).text();
         
-        if (score >= fTColor1) {
+        if (value >= fTcolor1) {
             $(this).addClass('fTcolor1');
         }
-        else if (score < fTColor1 && score >= fTColor2) {
-            $(this).addClass('good');
+        else if (value < fTcolor1 && value >= fTcolor2) {
+            $(this).addClass('fTcolor3');
         }
-        else if (score <good && score >= avg) {
-            $(this).addClass('avg');
+        else if (value <fTcolor3 && value >= fTcolor4) {
+            $(this).addClass('fTcolor4');
         }
-        else if (score < avg&& score >= poor) {
-            $(this).addClass('poor');
+        else if (value < fTcolor4&& value >= fTcolor5) {
+            $(this).addClass('fTcolor5');
         }
-        else if (score < poor) {
-            $(this).addClass('vPoor');
+        else if (value < fTcolor5) {
+            $(this).addClass('fTcolor6');
         }
         });});
